704388a38f9242684e12778acd57b6ed9d4436d1
Austin Burkeen <45767276+burkeena@users.noreply.github.com>
2023-02-20 02:57:43 -0500

    Update creator.py

--- creator.py
+++ creator.py
@@ -14,28 +14,21 @@ for filename in database_files_path:
     print(filename)
 print("\n") ## Blank line
 
-# #res_str_relative_path = database_files_path.translate({ord('/'): None})
-# print(res_str_relative_path)
-
-## def file_check_function():
-#    print(os.system("ls -la" + res_str_relative_path))
-
-# file_check_function()
 
 db_file = input("What database do you want to connect to? ") # Accept user input to define file name
 full_path = os.path.join(absolute_path, relative_path, db_file)
 print("\n") ## Blank line
-
 
 def create_sql_connection(db_file):
     """ create a connection to local database """
     conn = None
     try: 
         conn = sqlite3.connect(db_file)
-        print("Connected to SQLite Database..")
+        cur = conn.cursor()
+        print("Connected to SQLite Database.... " + db_file)
+        print("\n") ## Blank line
         print("The current version of this database is: " + sqlite3.version)
 
-        
     except Error as e:
         print(e)
     finally: 
